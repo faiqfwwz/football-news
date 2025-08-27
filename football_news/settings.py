33518ce21b52499fe8bdb2d9baf3b69b650d1cdd
faiqfwwz <ahmad.faiq41@ui.ac.id>
2025-08-27 16:16:38 +0700

    tes deploy

--- football_news/settings.py
+++ football_news/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+# test
 from pathlib import Path
 
 import os
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","ahmad-faiq41-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-faiq41-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
